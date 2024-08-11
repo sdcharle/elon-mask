1b9058eeb2a6b580c6b9123c087873e0624bdfc8
Stephen Charlesworth <sdcharle@Stephens-MacBook-Pro.local>
2024-08-11 15:13:44 -0400

    initial

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # elon-mask
+
+Get rid of E*** M*** references.
---
+++ background.js
@@ -0,0 +1,18 @@
+let isEnabled = true; // Track whether the extension is enabled or not
+
+chrome.action.onClicked.addListener((tab) => {
+    isEnabled = !isEnabled; // Toggle the enabled state
+
+    // Update the icon to reflect the enabled/disabled state
+    chrome.action.setIcon({
+        path: {
+            "16": isEnabled ? "icons8-car-16.png" : "icons8-poop-16.png",
+            "48": isEnabled ? "icons8-car-48.png" : "icons8-poop-48.png",
+            "128": isEnabled ? "icons8-car-128.png" : "icons8-poop-128.png"
+        }
+    });
+    
+
+    // Send a message to the content script to enable/disable it
+    chrome.tabs.sendMessage(tab.id, { action: isEnabled ? "enable" : "disable" });
+});
